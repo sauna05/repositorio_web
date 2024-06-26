a444d4d55d6082cf4a0cafebb68457c07080e269
Alexander Sauna <alexandersauna05@gmail.com>
2024-06-26 08:51:53 -0500

    Se agrego el boceto y los utlimos cambios a repositorio web de la ferreteria

---
+++ proyecto_canino/slider.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const slides = document.querySelectorAll(".slides img");
+  let currentSlide = 0;
+
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      if (i === index) {
+        slide.style.display = "block";
+      } else {
+        slide.style.display = "none";
+      }
+    });
+  }
+
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+  }
+
+  showSlide(currentSlide); 
+  setInterval(nextSlide, 4000);
+});
